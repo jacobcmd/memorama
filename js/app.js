76d78e7dbbc750cd33523b7008891bb78f4397cc
Jacob Mendoza <jacob.mendoza.hdz@gmail.com>
2022-10-27 21:47:10 -0500

    segunda entrega

---
+++ js/app.js
@@ -0,0 +1,170 @@
+let parejasAcertadas = [];
+let numImgVisibles = 0;
+
+let puntos = 0;
+let partidaIniciada = false;
+window.onload = grid;
+
+function grid() {
+	cargarImagenes();
+	generarCartas(4, 8, coches);
+}
+
+function generarCartas(columnasFilas, numImg, tematica) {
+	let parentElement = document.getElementById("wrapper");
+	let numElements = columnasFilas * columnasFilas;
+	let listaImagenes = imagenes(numImg, tematica);
+
+	for (let i = 0; i < numElements; i++) {
+		let img = document.createElement('INPUT');
+		img.setAttribute("type", "image");
+		img.setAttribute("class", "imagenCarta");
+		img.setAttribute("visible", false);
+		img.setAttribute("src", listaImagenes[i]);
+		carta(parentElement, img, numImg);
+	}
+
+	parentElement.style.setProperty('--rowNum', columnasFilas);
+	parentElement.style.setProperty('--colNum', columnasFilas);
+}
+
+function carta(contenedor, img, numImg) {
+	let carta = document.createElement('DIV');
+	carta.setAttribute("class", "carta");
+	contenedor.appendChild(carta);
+
+	let front = document.createElement('DIV');
+	front.setAttribute("class", "front face");
+	carta.appendChild(front);
+	front.appendChild(img);
+
+	let back = document.createElement('DIV');
+	back.setAttribute("class", "back face");
+	carta.appendChild(back);
+
+	let imgReverso = document.createElement('INPUT');
+	imgReverso.setAttribute("type", "image");
+	imgReverso.setAttribute("src", "img/reverso.png");
+	back.appendChild(imgReverso);
+
+	carta.onclick = function () {
+		if (img.getAttribute("visible") == "false") {
+			carta.classList.add("mostrar");
+			img.setAttribute("visible", true);
+			numImgVisibles++;
+
+			comprobarParejas();
+
+			if (parejasAcertadas.length == numImg) {
+				
+			}
+		}
+	}
+}
+
+function imagenes(numImg, tematica) {
+	let imagenes = [];
+	let i = 0;
+	while (i < numImg) {
+		let nuevaImagen = tematica[getAleatorio(tematica)];
+		if (!imagenes.includes(nuevaImagen)) {
+			imagenes[i] = nuevaImagen;
+			i++;
+		}
+	}
+	return mezclarImagenes(imagenes, numImg);
+}
+
+function mezclarImagenes(imagenes, numImg) {
+	let baraja = [];
+	baraja.length = numImg * 2;
+
+	let i = 0
+	while (i < baraja.length) {
+		let nuevaImagen = imagenes[getAleatorio(imagenes)];
+		if (!baraja.includes(nuevaImagen) || contarRepeticiones(baraja, nuevaImagen) < 2) {
+			baraja[i] = nuevaImagen;
+			i++;
+		}
+	}
+	return baraja;
+}
+
+function contarRepeticiones(lista, imagen) {
+	let repeticiones = 0;
+	for (let i = 0; i < lista.length; i++) {
+		if (lista[i] == imagen) {
+			repeticiones++;
+		}
+	}
+	return repeticiones;
+}
+
+function comprobarParejas() {
+	if (numImgVisibles == 2) {
+		bloquearPanel(true);
+
+		let parejas = [];
+		numImgVisibles = 0;
+
+		let imagenes = document.getElementsByClassName("imagenCarta");
+		for (let i = 0; i < imagenes.length; i++) {
+			if (!parejasAcertadas.includes(imagenes[i].getAttribute("src")) & imagenes[i].getAttribute("visible") == "true") {
+				parejas.push(imagenes[i]);
+			}
+		}
+
+		if (parejas[0].getAttribute("src") != parejas[1].getAttribute("src")) {
+			
+				puntos++;
+			setTimeout(
+				function () {
+					girarParejas(parejas[0], parejas[1]);
+					setTimeout(function () {
+						bloquearPanel(false);					
+					}, 1000);
+				},
+				1000
+			);
+		}
+		else {
+			parejasAcertadas.push(parejas[0].getAttribute("src"));
+			puntos ++;
+			bloquearPanel(false);
+			
+		}
+		scorePartida();
+	}
+}
+
+function girarParejas(pareja1, pareja2) {
+	pareja1.closest(".carta").classList.remove("mostrar");
+	pareja1.classList.add("ocultar");
+	pareja1.setAttribute("visible", false);
+
+	pareja2.closest(".carta").classList.remove("mostrar");
+	pareja2.classList.add("ocultar");
+	pareja2.setAttribute("visible", false);
+}
+
+function getAleatorio(tematica) {
+	return Math.floor(Math.random() * (tematica.length - 0));
+}
+
+function bloquearPanel(bloquear) {
+	let tablero = document.getElementById("wrapper");
+	if (bloquear)
+		tablero.classList.add("bloquear");
+	else
+		tablero.classList.remove("bloquear");
+
+	let imagenes = document.getElementsByClassName("imagenCarta");
+	for (let i = 0; i < imagenes.length; i++) {
+		imagenes[i].disabled = bloquear;
+	}
+}
+
+function scorePartida() {
+	let divScore = document.getElementById("puntosValue");
+	divScore.innerHTML = puntos;
+}
---
+++ js/imagenes.js
@@ -0,0 +1,49 @@
+let coches = [];
+let reverso = "img/reverso.png";
+
+function cargarImagenes() {
+
+	coches[0] = "img/coches/alfaromeogiulia.jpg";
+	coches[1] = "img/coches/alfaromeogiulieta.jpg";
+	coches[2] = "img/coches/audia4.jpg";
+	coches[3] = "img/coches/audia6.jpg";
+	coches[4] = "img/coches/bmwserie5.jpg";
+	coches[5] = "img/coches/bmwx3.jpg";
+	coches[6] = "img/coches/citroencactus.jpg";
+	coches[7] = "img/coches/citroenpicasso.jpg";
+	coches[8] = "img/coches/ds3.jpg";
+	coches[9] = "img/coches/ds5.jpeg";
+	coches[10] = "img/coches/fiat500.jpg";
+	coches[11] = "img/coches/fiatpanda.jpg";
+	coches[12] = "img/coches/fordfiesta.jpg";
+	coches[13] = "img/coches/fordfocus.jpg";
+	coches[14] = "img/coches/hyundaii30.jpg";
+	coches[15] = "img/coches/hyundaitucson.jpg";
+	coches[16] = "img/coches/kiario.jpg";
+	coches[17] = "img/coches/kiasportage.jpg";
+	coches[18] = "img/coches/mazda2.jpg";
+	coches[19] = "img/coches/mazda3.jpg";
+	coches[20] = "img/coches/mercedesclasee.jpg";
+	coches[21] = "img/coches/mercedesclases.jpg";
+	coches[22] = "img/coches/nissanleaf.png";
+	coches[23] = "img/coches/nissanqashqai.jpg";
+	coches[24] = "img/coches/opelcorsa.jpg";
+	coches[25] = "img/coches/opelinsignia.jpg";
+	coches[26] = "img/coches/peugeot5008.jpg";
+	coches[27] = "img/coches/peugeot508.jpg";
+	coches[28] = "img/coches/porsche911.jpg";
+	coches[29] = "img/coches/porschecayenne.jpg";
+	coches[30] = "img/coches/renault captur.jpg";
+	coches[31] = "img/coches/renault kadjar.jpeg";
+	coches[32] = "img/coches/seatleon.jpg";
+	coches[33] = "img/coches/seattarraco.jpg";
+	coches[34] = "img/coches/skodakaroq.jpg";
+	coches[35] = "img/coches/skodaoctavia.jpg";
+	coches[36] = "img/coches/toyotaauris.jpg";
+	coches[37] = "img/coches/toyotaprius.jpg";
+	coches[38] = "img/coches/volkswagenpolo.jpeg";
+	coches[39] = "img/coches/volkswagentroc.jpg";
+	coches[40] = "img/coches/volvov40.jpg";
+	coches[41] = "img/coches/volvoxc40.jpg";
+
+}
